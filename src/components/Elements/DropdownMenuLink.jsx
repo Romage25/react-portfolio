a77a30160a2ac98ecf30577922e97cda63731c98
Rodel Gerodias <rodelgerodias25@gmail.com>
2023-10-13 09:46:07 +0800

    increase padding dropdown menu

--- src/components/Elements/DropdownMenuLink.jsx
+++ src/components/Elements/DropdownMenuLink.jsx
@@ -1,7 +1,7 @@
 import { NavItem } from "./NavItemElements";
 
 export const DropdownMenuLink = () => (
-  <ul className="mt-3 p-2 shadow w-20 text-right bg-white rounded-md absolute right-0">
+  <ul className="mt-3 p-3 shadow w-20 text-right bg-white rounded-md absolute right-0">
     <NavItem link="#home" text="Home" />
     <NavItem link="#about" text="About" />
     <NavItem link="#resume" text="Resume" />
